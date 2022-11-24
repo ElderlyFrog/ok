9a63a9046c672d927eddf90b758e6d5166d2b24a
ElderlyFrog <113665414+ElderlyFrog@users.noreply.github.com>
2022-11-24 23:34:08 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {useEffect} from "react";
+import './App.css'
+//import SearchIcon from './search.svg'
+const API_URL ='https://www.omdbapi.com?apikey=9a0a62f1'
+
+
+const App = () => {
+    const searchMovies =  async(title) => {
+     const response = await fetch(`${API_URL}&s=${title}`)
+     const data = await response.json()
+     console.log(data.Search)
+    }
+    
+    useEffect(() => {
+      searchMovies('spiderman')
+        },[])
+    return (
+        <div className={"app"}>
+           <h1>MovieLand</h1>
+
+            <div className="search">
+               <input
+                   placeholder={"Search for movies"}
+               value="Superman"
+                   onChange={()=>{}}
+               />
+           </div>
+
+        </div>
+    )
+}
+export default  App
